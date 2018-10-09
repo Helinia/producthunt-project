9d8c649af5ea51738190f82be217260be0a7126b
Helinia <yex@mcmaster.ca>
2018-10-09 00:14:40 -0300

    added authentication system for user login and logout as well as corresponding webpages

--- account/views.py
+++ account/views.py
@@ -1,1 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        #user has info and wants an account.
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'account/signup.html',{'error':'user has already been taken!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request, 'account/signup.html',{'error':'Password must match.'})
+    else:
+        #user wants to enter info
+        return render(request, 'account/signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'account/login.html',{'error':'username or password is incorrect'})
+
+
+    else:
+        return render(request, 'account/login.html')
+
+
+def logout(request):
+    #Have to make sure that it is a POST request!
+    #Since Google Chrome will actively pre-load the webpages.
+    #If it is a GET request, it will automatically log out the user.
+
+    #TODO: need to route to homepage and don't forget to logout.
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
--- producthunt/urls.py
+++ producthunt/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name = 'home'),
-    
+    path('account/', include('account.urls')),
 ]
