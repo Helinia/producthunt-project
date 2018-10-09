84c76bb97872ee6864fb2ff2da122ede18632ebd
Helinia <yex@mcmaster.ca>
2018-10-09 00:51:56 -0300

    register product model in admin

--- product/models.py
+++ product/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+
+
+class Product(models.Model):
+    #title
+    title = models.CharField(max_length = 255)
+    #url
+    url = models.TextField()
+    #pub_date
+    pub_date = models.DateTimeField()
+    #votes_total
+    votes_total = models.IntegerField(default = 1)
+    #image
+    image = models.ImageField(upload_to = 'images/')
+    #icon
+    icon = models.ImageField(upload_to = 'images/')
+    #body
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
+    def summary(self):
+        return self.body[:100]
